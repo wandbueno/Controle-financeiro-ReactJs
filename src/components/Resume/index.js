9dd798a6bd9845f739549186c8b7d1cd706547cb
Wanderson Bueno <wanolibueno@gmail.com>
2022-05-07 19:22:56 -0300

    imagens do projeto

---
+++ src/components/Resume/index.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import ResumeItem from '../ResumeItem'
+import * as C from './styles'
+
+import {
+  FaRegArrowAltCircleUp,
+  FaRegArrowAltCircleDown,
+  FaDollarSign
+} from 'react-icons/fa'
+
+const Resume = ({ income, expense, total }) => {
+  return (
+    <C.Container>
+      <ResumeItem
+        title="Entradas"
+        Icon={FaRegArrowAltCircleUp}
+        value={income}
+      />
+      <ResumeItem
+        title="Saidas"
+        Icon={FaRegArrowAltCircleDown}
+        value={expense}
+      />
+      <ResumeItem title="Total" Icon={FaDollarSign} value={total} />
+    </C.Container>
+  )
+}
+
+export default Resume
